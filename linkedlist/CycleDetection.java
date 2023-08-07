d39293e4ff3bde7d37574b3fcf3cbdbc25fe9146
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-07 18:42:40 +0530

    Cycle Detection

---
+++ linkedlist/CycleDetection.java
@@ -0,0 +1,28 @@
+// fast and slow pointer
+
+public class CycleDetection {
+  private class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public boolean detectCycle(ListNode head) {
+    ListNode fast, slow;
+    slow = head;
+    fast = head;
+
+    while (slow != null && fast != null && fast != slow) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    if (slow == null || fast == null) {
+      return true;
+    }
+    return false;
+  }
+}
